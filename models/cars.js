52f34bf4b5721c2c232a877353702116884c7c83
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2017-05-12 16:41:31 +0200

    models sketches added

---
+++ models/cars.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+let carSchema = new Schema(
+    {
+        brand: {
+            type: String,
+            required: true
+        },
+        automat: {
+            type: Boolean,
+            required: true,
+        },
+        roof_rack: {
+            type: Boolean,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        booked: {
+            type: Boolean,
+            required: true
+        },
+        seats: {
+            type: Number,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+let Cars = mongoose.model('Car', carSchema);
+module.exports = Cars;
---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose').require('mongoose-type-email');
+Schema = mongoose.Schema;
+
+
+
+let userSchema = Schema(
+    {
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: mongoose.SchemaTypes.Email, 
+            required: true
+        }
+    }
+)
