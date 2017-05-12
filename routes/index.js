3b2da63581073fb0be28babe5e0a5f3287d04b76
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2017-05-12 18:17:00 +0200

    Connection to database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose'),
+  url = 'mongodb://admin:admin@ds139791.mlab.com:39791/bilbokning';
+
+mongoose.connect(url);
+
+mongoose.connection.on('connected', () => {
+  console.log("Connected to database");
+})
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+router.get('/cars/car/:id', function (req, res, next) {
+
+  const carID = req.params.id;
+  console.log(`Detta id fick vi ${carID}`);
+
+  res.render('index', {
+    title: 'Express',
+    carID: carID
+  });
 });
 
 module.exports = router;
