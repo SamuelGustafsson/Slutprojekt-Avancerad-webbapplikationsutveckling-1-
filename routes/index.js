e78a577569def559cd8eef97c4b2a52086e0719c
Mats Wikmar <matwik@gmail.com>
2017-05-17 23:28:20 +0200

    added headermenu dummy car insert,delete and a dummy datepicker

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 let express = require('express');
 let router = express.Router();
 
+const Cars  = require("../models/cars");
+const Users = require("../models/users");
+
 
 /* GET home page. */
 router.get('/', function (req, res, next){
 
+
   const {formError, authError} = req.query;
   res.render('index', {
-    title: 'Bilbokningen - login',
+    title: 'Car Rental Supreme - login',
     formError: formError,
     authError: authError,
   });
@@ -20,6 +24,29 @@ router.get('/signup', function (req, res, next){
   res.render('signup', {
     formError: formError
   });
+});
+
+
+
+/* dummy routes start */
+router.get('/dummy/cars/insert', function (req, res, next){
+
+  const json = require("../dummy-cars.json");
+  Cars.create(json, (err, car) => {
+    if (err) throw err;
+    console.log("-- /dummy/cars/insert DONE --");
+    res.redirect("/");
+  });
+
+});
+router.get('/dummy/cars/delete', function (req, res, next){
+
+  Cars.remove({},(err, car) => {
+    if (err) throw err;
+    console.log("-- /dummy/cars/delete DONE --");
+    res.redirect("/");
+  });
+
 });
 
 module.exports = router;
