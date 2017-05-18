a2be1e44a38065aef529af3fbe40342b06904633
SamuelGustafsson <samuelgustafsson890623@gmail.com>
2017-05-18 20:53:56 +0200

    nyc added to project

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,8 +2,6 @@ const expect  = require("expect");
 const request = require("supertest");
 const app     = require("../app");
 
-// Add dymmy cars here
-
 describe("GET /", () => {
 
   it("should get /", (done) => {
